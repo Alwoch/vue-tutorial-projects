67a5d6934d607d11b7439d2b73695267d0d5dc66
Alwoch <salwoch@gmail.com>
2024-07-18 13:57:14 +0300

    create vue counter app

---
+++ vueCounterApp/src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref } from 'vue';
+
+const count=ref(0)
+</script>
+<!-- Template -->
+<template>
+  <body>
+    <div>
+      <h4>The current count is .....</h4>
+      <h1>{{ count }}</h1>
+      <button @click="count--" id="reduceCount">-</button>
+      <button @click="count++" id="increaseCount">+</button>
+    </div>
+  </body>
+</template>
+
+<!-- Styles -->
+<style scoped>
+body {
+  width: 100vw;
+  height: 100vh;
+  background-color: aliceblue;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+div {
+  text-align: center;
+}
+</style>
